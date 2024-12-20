801e5769eda029f882b6daff0c50d3c625b8ebad
liliz <liliz@example.es>
2024-12-20 07:35:06 +0100

    add swagger config

--- src/main/java/es/lili/simple/samples/model/Genre.java
+++ src/main/java/es/lili/simple/samples/model/Genre.java
@@ -3,6 +3,7 @@ package es.lili.simple.samples.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Setter
@@ -15,9 +16,14 @@ import lombok.*;
 public class Genre {
 
     @JsonProperty("id")
+    @Schema(name = "id", description = "Genre unique id", type = "Long", example = "5",
+            accessMode = Schema.AccessMode.READ_ONLY)
     private Long idGenre;
     @JsonProperty("name")
+    @Schema(name = "name", description = "Genre name", type = "String", example = "FANTASY")
     private String name;
     @JsonProperty("description")
+    @Schema(name = "description", description = "Genre description", type = "String",
+            example = "Fictional worlds and magic")
     private String description;
 }
--- src/main/java/es/lili/simple/samples/web/genres/GenreController.java
+++ src/main/java/es/lili/simple/samples/web/genres/GenreController.java
@@ -1,23 +1,36 @@
 package es.lili.simple.samples.web.genres;
 
 import es.lili.simple.samples.model.Genre;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RequestMapping("/genres")
+@Tag(name = "Genre Management", description = "This API allows the user to manage the Genres")
 public interface GenreController {
 
+    @Operation(description = "Retrieve all genres that could be assigned to a book",
+            method = "GET", summary = "Retrieve all genres")
     Flux<Genre> genres();
 
+    @Operation(description = "Retrieve genre by its unique id",
+    method = "GET", summary = "Retrieve genre by id")
     Mono<Genre> genreById(Long id);
 
+    @Operation(description = "Retrieve genres that have a matching name",
+    method = "GET", summary = "Retrieve genre by name")
     Flux<Genre> genreFiltered(String name);
 
+    @Operation(description = "Register new Genre in database", method = "POST", summary = "Add genre")
     Mono<Genre> addGenre(Genre genre);
 
+    @Operation(description = "Update genre's name or description", method = "PATCH", summary = "Update genre")
     Mono<Genre> updateGenre(Genre genre);
 
+    @Operation(description = "Delete genre so it cannot be used to classify books",
+    method = "DELETE", summary = "Delete genre")
     Mono<Genre> deleteGenre(Genre genre);
 
 }
